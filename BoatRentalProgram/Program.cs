a93c3536971ab78c48384c8f344b4c2c96513a03
stcrownclown <i_am_crownclown@hotmail.com>
2019-06-30 00:05:36 +0700

    init before sleep

---
+++ BoatRentalProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoatRentalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
